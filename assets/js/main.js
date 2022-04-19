28afafcc4b3cf268b0646e3f7fdd11933920d8ce
J-O-B <Jonathanmichaelobrien@gmail.com>
2022-04-19 19:14:55 +0000

    updated aria label on news script modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -100,11 +100,11 @@ $.getJSON('https://nci-ca-api.herokuapp.com/news', function(data){
                 </button>
 
                 <!-- Modal -->
-                <div class="modal fade" id="example${i}Modal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal fade" id="example${i}Modal" tabindex="-1" aria-labelledby="example${i}ModalLabel" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable modal-xl">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">${title}</h5>
+                                <h5 class="modal-title" id="example${i}ModalLabel">${title}</h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
